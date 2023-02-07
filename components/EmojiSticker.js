03891439894f2bfb8ccdc3b18c9c3be167bff5ff
bronifty <bronifty@gmail.com>
2023-02-06 21:36:19 -0500

    .

--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,9 +1,31 @@
 import { View, Image } from "react-native";
 import Animated from "react-native-reanimated";
+import { TapGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  useAnimatedGestureHandler,
+  withSpring,
+} from "react-native-reanimated";
 
 const AnimatedImage = Animated.createAnimatedComponent(Image);
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
+  const scaleImage = useSharedValue(imageSize);
+  const onDoubleTap = useAnimatedGestureHandler({
+    onActive: () => {
+      if (scaleImage.value) {
+        scaleImage.value = scaleImage.value * 2;
+      }
+    },
+  });
+  const imageStyle = useAnimatedStyle(() => {
+    return {
+      width: withSpring(scaleImage.value),
+      height: withSpring(scaleImage.value),
+    };
+  });
+
   return (
     <View style={{ top: -350 }}>
       <AnimatedImage
